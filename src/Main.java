bf96560716d71186a7e032807bcae38b1112ad85
Raphael Tango05 <raphael.tango05@gmail.com>
2022-11-15 20:58:12 +0100

    finish

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
+    private static final double PI = Math.PI;
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("This is just an output!");
+    }
+
+    public static double area (double radius) {
+        if (radius < 0) return -1;
+
+        return (radius * radius) * PI;
+    }
+
+    public static double area (double x, double y) {
+        if( x<0 || y<0 ) return -1;
+
+        return x * y;
     }
 }
